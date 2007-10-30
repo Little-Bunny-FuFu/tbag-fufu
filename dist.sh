cf982ad65da8bbbc1e87f1641a31fe5c35cb72f1
shefki <shefki@b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806>
2007-10-30 10:49:01 +0000

    * dist.sh: Update to use svnversion and change filename format slightly.
    git-svn-id: https://repos.curseforge.com/wow/tbag-shefki/trunk@23 b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806

--- dist.sh
+++ dist.sh
@@ -6,8 +6,8 @@ version=`grep -E '^TBAG_VERSION = ' localization.lua | cut -d '"' -f 2`
 year=`echo ${version} | cut -d '-' -f 1`
 month=`echo $version | cut -d '-' -f 2`
 day=`echo $version | cut -d '-' -f 3`
-beta=`echo $version | cut -d '-' -f 4 | grep beta`
-zipfile="TBag-$year$month$day$beta-Shefki.zip"
+rev=`svnversion`
+zipfile="TBag-Shefki-$year$month$day-r$rev.zip"
 
 if [ -e ~/Desktop/$zipfile ]; then
   rm -f ~/Desktop/$zipfile
