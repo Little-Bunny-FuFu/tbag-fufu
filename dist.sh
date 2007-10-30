ca50dc5ede5e3c5a8f35b184eb7119debabf3f34
shefki <shefki@b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806>
2007-10-30 23:50:15 +0000

    * dist.sh: Take the first arg as the release type e.g. (-Alpha, -Beta).   No arg means normal release.
    git-svn-id: https://repos.curseforge.com/wow/tbag-shefki/trunk@31 b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806

--- dist.sh
+++ dist.sh
@@ -2,9 +2,10 @@
 
 # $Id$
 
+dev="$1"
 rev=`perl -pi -e 'BEGIN{ $rev = qx(svnversion); print $rev; chomp $rev} s/^(local rev = \\x27\\$Rev: ).*?\\$/$1$rev \\$/' localization.lua`
 date=`perl '-MPOSIX qw(strftime)' -pi -e 'BEGIN { $now = time(); $timestr = strftime("%Y-%m-%d %H:%M:%S %z (%a, %d %b %Y)", localtime($now)); print strftime("%Y%m%d\n",localtime($now));} s/^(local date = \\x27\\$Date: ).*?\\$/$1$timestr \\$/' localization.lua`
-dev=`perl -pi -e 'if (/^local dev = \\x27.*?\\x27/) { s/-Alpha//; m/\\x27(.*?)\\x27/; print STDOUT $1,$/}' localization.lua`
+perl -pi -e "s/^(local dev = \\x27).*?\\x27/\$1$dev\\x27/" localization.lua
 zipfile="TBag-Shefki-$date-r$rev$dev.zip"
 
 if [ -e ~/Desktop/$zipfile ]; then
