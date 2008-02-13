90434d312ed81ec94c02e73de7ebc70894521687
shefki <shefki@b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806>
2008-02-13 00:09:39 +0000

    * TInvHooks.lua   TInvHooks_ContainerFrameItemButton_OnModifiedClick():     Stop calling BankFrameItemButtonGeneric_OnModifiedClick() when at the bank.     This is causing the event to be processed twice for some reason.
    git-svn-id: https://repos.curseforge.com/wow/tbag-shefki/trunk@47 b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806

--- TInvHooks.lua
+++ TInvHooks.lua
@@ -548,7 +548,6 @@ function TInvHooks_ContainerFrameItemButton_OnModifiedClick(button)
       end
     elseif (TBNK_ATBANK == 1 and itm[TBAG_I_BAG] == -1) then
       TBag_PrintDEBUG('ItemButton_OnModifiedClick At bank click on bank bag');
-      BankFrameItemButtonGeneric_OnModifiedClick(button);
       call_blizzard = false;
     end
   end
