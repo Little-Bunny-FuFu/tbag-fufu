251f1882db7037617c753be29863a7e2858824a0
shefki <shefki@b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806>
2008-02-18 01:04:13 +0000

    * dist.sh: Versioning info moved to TBag.lua so update dist.sh to match.
    git-svn-id: https://repos.curseforge.com/wow/tbag-shefki/trunk@62 b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806

--- dist.sh
+++ dist.sh
@@ -3,9 +3,9 @@
 # $Id$
 
 dev="$1"
-rev=`perl -pi -e 'BEGIN{ $rev = qx(svnversion); print $rev; chomp $rev} s/^(local rev = \\x27\\$Rev: ).*?\\$/$1$rev \\$/' localization.lua`
-date=`perl '-MPOSIX qw(strftime)' -pi -e 'BEGIN { $now = time(); $timestr = strftime("%Y-%m-%d %H:%M:%S %z (%a, %d %b %Y)", localtime($now)); print strftime("%Y%m%d\n",localtime($now));} s/^(local date = \\x27\\$Date: ).*?\\$/$1$timestr \\$/' localization.lua`
-perl -pi -e "s/^(local dev = \\x27).*?\\x27/\$1$dev\\x27/" localization.lua
+rev=`perl -pi -e 'BEGIN{ $rev = qx(svnversion); print $rev; chomp $rev} s/^(local rev = \\x27\\$Rev: ).*?\\$/$1$rev \\$/' TBag.lua`
+date=`perl '-MPOSIX qw(strftime)' -pi -e 'BEGIN { $now = time(); $timestr = strftime("%Y-%m-%d %H:%M:%S %z (%a, %d %b %Y)", localtime($now)); print strftime("%Y%m%d\n",localtime($now));} s/^(local date = \\x27\\$Date: ).*?\\$/$1$timestr \\$/' TBag.lua`
+perl -pi -e "s/^(local dev = \\x27).*?\\x27/\$1$dev\\x27/" TBag.lua
 zipfile="TBag-Shefki-$date-r$rev$dev.zip"
 
 if [ -e ~/Desktop/$zipfile ]; then
