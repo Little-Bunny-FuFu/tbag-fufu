8e3d6bd8a7f8d822b89f94fe2655a9a41c2a5b27
shefki <shefki@b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806>
2015-01-06 02:55:03 +0000

    Fix a copy-paste bug in the enUS locale, everything should be flagged as true.
    git-svn-id: https://repos.curseforge.com/wow/tbag-shefki/trunk@507 b5ab3ea7-6efc-44a1-92e0-c4a1a41d0806

--- localization.enUS.lua
+++ localization.enUS.lua
@@ -293,7 +293,7 @@ L["LTHR"] = true
 L["PET"] = true
 L["INSC"] = true
 L["TACKLE"] = true
-L["REAGENT"] = false
+L["REAGENT"] = true
 L["FRIDGE"] = true
 
 -- Bag Position Names, also used for EMPTY_%s_SLOTS and IN_%s_BAG categories
